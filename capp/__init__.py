8d9d7c7cc65f418d741af9c7dcca0ed431f6a65c
Demonstrandum1 <ole.eiane@outlook.com>
2023-03-07 22:36:59 +0100

    Lagt til alt av forms. Mangler å endre på CSS

--- capp/__init__.py
+++ capp/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 
 application = Flask(__name__)
 
+application.config['SECRET_KEY'] = '7bfd2e9c432da6e54b071b7828d97a22a02012d451e082a8'
+
 from capp.home.routes import home
 from capp.methodology.routes import methodology
 from capp.carbon_app.routes import carbon_app
